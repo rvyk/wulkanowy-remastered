ad2e28bea736e3a68d029fbe44ca5c4572d4f897
Paweł Szymański <szymanskipawel327@gmail.com>
2024-05-19 17:01:14 +0200

    feat: support for destinationUrl in messages

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,21 +1,28 @@
+import { cn } from "@/lib/utils";
+import { WulkanowyMessages } from "@/types/wulkanowy";
 import { X } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const Message: React.FC = () => {
-  const [message, setMessage] = useState<{ content: string; id: number }>();
+  const [message, setMessage] = useState<WulkanowyMessages>();
   const hiddenMessages =
     JSON.parse(localStorage.getItem("hiddenMessages") || "[]") || [];
 
   useEffect(() => {
     fetch("https://messages.wulkanowy.net.pl/v1.json").then((response) => {
-      response.json().then((data) => {
-        if (
-          hiddenMessages.includes(data[0].id) ||
-          !data[0].messageTypes.includes("GENERAL_MESSAGE")
-        )
-          return;
+      response.json().then((data: WulkanowyMessages[]) => {
+        const filteredMessages = data.filter(
+          (message) => !message.targetFlavor
+        );
+        const firstMessage = filteredMessages[0];
 
-        setMessage(data[0]);
+        if (
+          firstMessage &&
+          !hiddenMessages.includes(firstMessage.id) &&
+          firstMessage.isVisible
+        ) {
+          setMessage(firstMessage);
+        }
       });
     });
 
@@ -40,8 +47,16 @@ const Message: React.FC = () => {
       data-aos-offset="-500"
       className="flex justify-center duration-500 items-center fixed bottom-6 z-50 w-full"
     >
-      <div className="bg-secondaryFixed relative text-onSecondaryFixed max-w-7xl text-center font-medium min-h-20 rounded-3xl flex items-center px-24">
-        <p>{message.content}</p>
+      <div className="relative bg-secondaryFixed text-onSecondaryFixed max-w-7xl text-center font-medium min-h-20 rounded-3xl flex items-center px-24">
+        <a
+          href={message.destinationUrl || ""}
+          target="_blank"
+          className={cn(
+            !message.destinationUrl && "cursor-default pointer-events-none"
+          )}
+        >
+          <p>{message.content}</p>
+        </a>
         <button onClick={handleClose} className="absolute right-8">
           <X />
         </button>
---
+++ src/types/wulkanowy.ts
@@ -0,0 +1,16 @@
+export type WulkanowyMessages = {
+  id: number;
+  title: string;
+  content: string;
+  types: string[];
+  messageTypes: string[];
+  priority: string;
+  destinationUrl: string;
+  versionMin: number;
+  versionMax: number;
+  targetFlavor: string;
+  isXVisible: boolean;
+  isOkVisible: boolean;
+  isDismissible: boolean;
+  isVisible: boolean;
+};
